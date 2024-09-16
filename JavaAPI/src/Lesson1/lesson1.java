c5fd7829ef7e4888a13da6d249363f8cc6db39c1
Your Name <you@example.com>
2024-09-16 13:08:55 +0300

    rename JavaAPI

--- JavaAPI/src/Lesson1/lesson1.java
+++ JavaAPI/src/Lesson1/lesson1.java
@@ -1,7 +1,11 @@
 package Lesson1;
 
+import java.util.Scanner;
+
 public class lesson1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("");
     }
 }
+
